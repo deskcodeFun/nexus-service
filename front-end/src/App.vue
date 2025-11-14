ef8dfcfdd85c8650877c3c99ca1388105fefe9c5
deskcodeFun <panya.korkusol@gmail.com>
2025-11-14 11:46:11 +0700

    add google meterial font + icons

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,87 +1,35 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <header>
-    <div class="flex flex-row ">
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        
-        <nav>
-          <RouterLink to="/">Home</RouterLink>
-          <RouterLink to="/about">About</RouterLink>
-        </nav>
+  <header class="flex flex-row m-16 bg-amber-100">
+    <RouterLink
+      to="/"
+      class="text-sm m-2 p-2 flex flex-col justify-center items-center hover:bg-amber-600"
+    >
+      <i class="material-symbols-outlined" style="font-size: 36px;font-weight: 300;"> home </i>
 
-    </div>
-      
+      Home
+    </RouterLink>
+    <RouterLink
+      to="#"
+      class="text-sm m-2 p-2 flex flex-col justify-center items-center hover:bg-amber-600"
+    >
+      <i class="material-symbols-outlined" style="font-size: 36px;font-weight: 300;">person </i>
 
- 
+      Staff
+    </RouterLink>
+    <RouterLink
+      to="/about"
+      class="text-sm m-2 p-2 flex flex-col justify-center items-center hover:bg-amber-600"
+    >
+      <i class="material-symbols-outlined" style="font-size: 36px;font-weight: 300;">info </i>
+      <span>About</span>
+    </RouterLink>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
